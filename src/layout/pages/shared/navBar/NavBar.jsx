a6b0b762bd8e6b1365b23c515e47c893184950dd
ahad1033 <a.ahad2232@gmail.com>
2023-06-01 01:21:38 +0600

    banner section added(74/3)

--- src/layout/pages/shared/navBar/NavBar.jsx
+++ src/layout/pages/shared/navBar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
 
     return (
         <>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 text-white bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
