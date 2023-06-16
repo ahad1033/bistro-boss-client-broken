cb169fa1e631aeaeab5d76c83c34ab6c7e882910
ahad1033 <a.ahad2232@gmail.com>
2023-06-16 17:14:39 +0600

    74-4 swiper section updated

--- src/layout/pages/home/Home/catagory/Catagory.jsx
+++ src/layout/pages/home/Home/catagory/Catagory.jsx
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
 
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper";
 
 import slide1 from '../../../../../assets/home/slide1.jpg';
 import slide2 from '../../../../../assets/home/slide2.jpg';
@@ -15,30 +15,34 @@ import slide5 from '../../../../../assets/home/slide5.jpg';
 const Catagory = () => {
     return (
         <Swiper
-            slidesPerView={"auto"}
+            slidesPerView={4}
             centeredSlides={true}
             spaceBetween={30}
             pagination={{
                 clickable: true,
             }}
             modules={[Pagination]}
-            className="mySwiper"
+            className="mySwiper mb-24"
         >
             <SwiperSlide>
                 <img src={slide1} alt="" />
-                <h3 className="text-4xl uppercase text-center -mt-16">salad</h3>
+                <h3 className="text-4xl uppercase text-center -mt-20 text-white">Salads</h3>
             </SwiperSlide>
             <SwiperSlide>
                 <img src={slide2} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-20 text-white">Pizza</h3>
             </SwiperSlide>
             <SwiperSlide>
                 <img src={slide3} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-20 text-white">Soups</h3>
             </SwiperSlide>
             <SwiperSlide>
                 <img src={slide4} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-20 text-white">Deserts</h3>
             </SwiperSlide>
             <SwiperSlide>
                 <img src={slide5} alt="" />
+                <h3 className="text-4xl uppercase text-center -mt-20 text-white">Salads</h3>
             </SwiperSlide>
         </Swiper>
     );
